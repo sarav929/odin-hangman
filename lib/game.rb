9f2db1617c1c6f03ceb861fc5f77301835f2eb01
Sara <123300249+sarav929@users.noreply.github.com>
2024-07-12 15:20:52 +0100

    implemented save/load game data functions optimised game logic and user prompts

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,53 @@
 class Game
-  def initialize(words, fails)
-    @words = words
+  def initialize(secret_word, fails, guess_array)
+    @secret_word = secret_word
     @fails = fails
+    @guess_array = guess_array
+  end
+
+  def save_game
+    data = {
+      secret_word: @secret_word,
+      fails: @fails,
+      guess_array: @guess_array
+    }
+    File.open("saved_game.yaml", "w") { |file| file.write(YAML.dump(data)) }
+    puts "Data saved correctly.\n"
+  end
+
+  def load_game
+    if File.exist?("saved_game.yaml")
+      data = YAML.load_file("saved_game.yaml")
+      @secret_word = data[:secret_word]
+      @fails = data[:fails]
+      @guess_array = data[:guess_array]
+      puts "Data loaded correctly.\n"
+    else
+      puts "No saved game found.\n"
+    end
   end
 
   def create_secret_word
-    @secret_word = @words[rand(@words.length)].chomp
+    @secret_word = WORDS_ARRAY[rand(WORDS_ARRAY.length)].chomp
   end
 
   def make_guess
-    if @empty_array == nil
-      @empty_array = []
+    if @guess_array == []
       @secret_word.length.times do 
-        @empty_array << "_"
+        @guess_array << "_"
       end
     end
-    puts "Make your guess:\n\n #{@empty_array.join}\n\n"
+    puts "Type (SAVE) to save the game or type a letter a-z to make your guess:\n\n #{@guess_array.join}\n\n"
 
     while @letter = gets.chomp.upcase
-      if !@letter.match?(/[A-Z]/) or @letter.length != 1
-        puts "Try again, type a letter from A to Z."
-      else
+      if @letter.match?(/[A-Z]/) and @letter.length == 1 
         return @letter
+        break
+      elsif @letter == "SAVE"
+        self.save_game
+        break
+      else
+        puts "Try again, type a letter from A to Z or (SAVE) to save the game."        
       end
     end
   end
@@ -32,32 +58,31 @@ class Game
     if @word_array.include?(@letter)
       @word_array.each_with_index do |char, index|
         if char == @letter
-          if @empty_array[index] == "_"
-            @empty_array[index] = @letter
+          if @guess_array[index] == "_"
+            @guess_array[index] = @letter
           else
             puts "You already got that right."
           end
         end
-      end 
-      
+      end        
     else
       @fails += 1
       puts "#{ascii_art[@fails - 1]}\n\n"
     end
-  end 
+  end
+
 
   def check_winner
-    if !@empty_array.include?('_')
-      puts "YOU WON!"
+    if !@guess_array.include?('_')
+      puts "\nYOU WON! The secret word was: #{@secret_word.upcase}"
       return true
     else
       return false
     end
   end
-
-  def check_fails 
+  def check_fails
     if @fails == MAX_FAILS
-      puts "YOU LOST. The secret word was: #{@secret_word}"
+      puts "\nYOU LOST. The secret word was: #{@secret_word.upcase}"
       return true
     else
       return false
@@ -65,18 +90,21 @@ class Game
   end
 
   def play(ascii_art)
-    while true
+    loop do
       make_guess
       if check_guess(ascii_art)
         if check_winner
-          break
+          return false
         end
       else
         if check_fails
-          break
+          return false
         end
       end
     end
   end
+
+
+  
 
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
 require_relative 'lib/game.rb'
+require 'yaml'
 require 'pry'
+
+WORDS_ARRAY = []
+file = File.readlines('google-10000-english-no-swears.txt')
+file.each do |el|
+  if el.length >= 5 && el.length <= 12
+    WORDS_ARRAY << el
+  end
+end
 
 ASCII_ART= ['''
  +----+
@@ -54,18 +63,24 @@ ASCII_ART= ['''
 
 MAX_FAILS = ASCII_ART.length
 
-words = []
-file = File.readlines('google-10000-english-no-swears.txt')
-file.each do |el|
-  if el.length >= 5 && el.length <= 12
-    words << el
+puts "Let's play Hangman! Would you like to\n(1) Start a new game\n(2) Load a saved game"
+while input = gets.chomp
+  if input != "1" and input != "2"
+    puts "Try again. Enter either 1 or 2."
+  else
+    hangman = Game.new("", 0, [])
+    secret_word = hangman.create_secret_word
+    if input == "2"
+      hangman.load_game
+    end
+    hangman.play(ASCII_ART)
+    break                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
   end
 end
 
-hangman = Game.new(words, 0)
 
-secret_word = hangman.create_secret_word
-hangman.play(ASCII_ART)
+
+
   
 
 
